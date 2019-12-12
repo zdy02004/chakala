de192ca3bdea3f30151daf7d920ae7c7e7a69558
触礁 <zdy0_2004@qq.com>
2019-12-13 01:04:13 +0800

    Update and rename restart.sh to start.sh

--- release/start.sh
+++ release/start.sh
@@ -2,17 +2,12 @@
 
 work_path=`pwd`
 
-while((1))
-do
-
 a=`ps -ef|grep chakala_go|grep -v grep|wc -l`
 
 if(($a==0))
 then
 nohup "$work_path""/chakala_go" &
 else 
-sleep 1
+echo "Have Started!"
 fi
-
-done
 
